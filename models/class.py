38c5da27baece173d3cdb304367c4d9c231f2453
Colin Cameron <ccameron2020@gmail.com>
2022-09-24 13:53:15 +0100

    Classes done

---
+++ models/class.py
@@ -0,0 +1,5 @@
+class Class:
+    def __init__(self, name, type ,id=None):
+        self.name= name
+        self.type= type
+        self.id= id
---
+++ models/member.py
@@ -0,0 +1,8 @@
+class Member:
+
+    def __init__(self, first_name, second_name, age, membership_type, id=None):
+        self.first_name= first_name
+        self.second_name= second_name
+        self.age= age
+        self.membership_type= membership_type
+        self.id = id
