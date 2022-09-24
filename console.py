5ad5b89134b4863fee6a92aad382a892d475c612
Colin Cameron <ccameron2020@gmail.com>
2022-09-24 14:53:38 +0100

    console basic set up

--- console.py
+++ console.py
@@ -5,9 +5,13 @@ from models.member import Member
 import reposirories.class_repository as class_repository 
 import reposirories.member_repository as member_repository
 
-class_repository.delete_all
-member_repository.delete_all
+class_repository.delete_all()
+member_repository.delete_all()
 
-
+member1 = Member('Vlad', "Von Carstein", 500, "standard")
+member2 = Member('Isabella', 'Von Carstein', 400, "standard")
+member_repository.save(member1, member2)
+class1 = Class("Yoga 101", 'Yoga')
+class_repository.save(class1)
 
 pdb.set_trace()
