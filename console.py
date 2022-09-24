264a81f6ebeda93c9059205b8d9f9ca95e02b64a
Colin Cameron <ccameron2020@gmail.com>
2022-09-24 14:39:36 +0100

    Delete all functions added

--- console.py
+++ console.py
@@ -1,4 +1,12 @@
 import pdb 
+from models.Class import Class
+from models.member import Member
+
+import reposirories.class_repository as class_repository 
+import reposirories.member_repository as member_repository
+
+class_repository.delete_all
+member_repository.delete_all
 
 
 
