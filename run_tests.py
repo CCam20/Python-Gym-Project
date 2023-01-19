395549b48c7b4b19e1f42e61d158101fec8dcfa2
Colin Cameron <ccameron2020@gmail.com>
2023-01-19 21:03:10 +0000

    Added Screenshots

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,6 @@
 import unittest
 
+from tests.member_tests import TestMember
+
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/member_tests.py
@@ -0,0 +1,11 @@
+import unittest
+from models.member import Member
+
+
+class TestMember(unittest.TestCase):
+
+    def setUp(self):
+       
+    def test_member_has_first_name(self):
+        self.assertEqual(, self.member.first_name)
+
